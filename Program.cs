ab1e03a6b64677f29c9ef6621b9c4e26aea9adc3
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-02-18 07:10:19 +0700

    Penambahan Angka

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World! 2");
+Console.WriteLine("Hello, World! 3");
+Console.WriteLine("Hello, World! 4");
+Console.WriteLine("Hello, World! 5");
